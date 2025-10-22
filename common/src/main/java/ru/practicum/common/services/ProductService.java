0f670853fc23e2e1a7257dbdbfd4dc71ca34d88b
Alexandr <lexfilonenko@yandex.ru>
2025-10-22 22:21:38 +0300

    Переработан сервис продукт

--- common/src/main/java/ru/practicum/common/services/ProductService.java
+++ common/src/main/java/ru/practicum/common/services/ProductService.java
@@ -1,18 +1,21 @@
 package ru.practicum.common.services;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ru.practicum.common.repository.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
     public List<String> searchProducts(String productName){
         log.info("🔍 Поиск товаров по имени: {}", productName);
-        //TODO Написать получение продуктов из БД
-        List<String> products = new ArrayList<>();
-        products.add("test");
 
-        return products;
+
+        return productRepository.searchProducts(productName);
     }
 }
