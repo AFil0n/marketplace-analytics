86ebf886b65036c960f331faae6263fb4fe0b8c8
Alexandr <lexfilonenko@yandex.ru>
2025-10-24 23:21:42 +0300

    Удалены лишние файлы из комон

--- common/src/main/java/ru/practicum/common/config/CommonConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.common.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.practicum.common")
-public class CommonConfig {
-}
