c096de5e545cbbe01c86847b57ab561b393bff5e
Alexandr <lexfilonenko@yandex.ru>
2025-10-24 23:49:44 +0300

    Исправление клиента

--- client/src/main/java/ru/practicum/client/ClientApplication.java
+++ client/src/main/java/ru/practicum/client/ClientApplication.java
@@ -1,13 +1,18 @@
 package ru.practicum.client;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import ru.practicum.common.config.CommonConfig;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Slf4j
-@Import(CommonConfig.class)
+@SpringBootApplication
+@EntityScan(basePackages = {
+        "ru.practicum.common.model",
+        "ru.practicum.client.model"
+})
+@EnableJpaRepositories(basePackages = {
+        "ru.practicum.client.repository"
+})
 public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
